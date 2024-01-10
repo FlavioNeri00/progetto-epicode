f9b9e0cbbb4589aadd96d175bf965d36de7b0a29
Flavio Neri <136917681+FlavioNeri00@users.noreply.github.com>
2024-01-10 13:46:21 +0100

    Update LawForm.jsx

--- src/Component/LawForm.jsx
+++ src/Component/LawForm.jsx
@@ -110,6 +110,14 @@ const LawForm = () => {
               </Form.Control.Feedback>
             </Form.Group>
 
+            <Form.Group md="3" controlId="validationCustom06" className=" mt-3">
+              <Form.Label>Raccontaci il problema*</Form.Label>
+              <Form.Control type="textarea" placeholder="" required />
+              <Form.Control.Feedback type="invalid">
+                Perfavore completa il form
+              </Form.Control.Feedback>
+            </Form.Group>
+
             <Form.Group className="mb-3 mt-3">
               <Form.Check
                 required
